fdd0328df41fe27f267a8ca07cdd2289b11096f5
boazandrew <andrewboaz417@gmail.com>
2025-06-19 18:12:40 +0530

    Second Commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const userRoute = require('./routes/user.route');
-const todoRoute = require('./routes/todo.route');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/user.route");
+const todoRoute = require("./routes/todo.route");
 
 dotenv.config();
 const app = express();
-app.use(cors());    
+app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI).then(()=> console.log("MongoDB Connected")).catch(err=>console.log('MongoDB Connection Error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
 
-app.use('/api/auth',userRoute);
-app.use('/api/todo', todoRoute);
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=> console.log(`Server Running on Port ${PORT}`));
+app.use("/api/auth", userRoute);
+app.use("/api/todo", todoRoute);
+
+app.get('/',(req,res)=> {
+    res.send("API is running");
+});
+
+module.exports = app;
